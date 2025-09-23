c1c3f48e6b5534ba2cb3f148b986359385b32dcf
Khayzannn <streamingattala@gmail.com>
2025-09-24 07:44:15 +0800

    FUFUFAFA

---
+++ Kelas/pertemuan-2/mainmain.py
@@ -0,0 +1,33 @@
+#k Budi ingin menghitung tagihan listrik bulanannya berdasarkan pemakaian kWh.
+# Dengan ketentuan tarif sebagai berikut:
+# Jika pemakaian ≤ 100 kWh maka tarif Rp 1.200 per kWh
+# Jika pemakaian 101 – 300 kWh maka tarif Rp 1.500 per kWh
+# Jika pemakaian > 300 kWh maka tarif Rp 2.000 per kWh
+
+
+
+
+# Rumus: Total Bayar = Pemakaian × Tarif per kWh
+
+# pemakaian = int(input("MASUKKAN KWh LISTRIK ANDA"))
+
+# total1 = pemakaian * 1200
+# total2 = pemakaian * 1500
+# total3 = pemakaian * 2000
+# if pemakaian <=100 :
+#     print(f'total tagihan anda {total1}')
+
+# elif pemakaian <=300:
+#     print(f'total tagihan anda {total2}')
+
+# elif pemakaian > 300:
+#     print(f'total tagihan anda {total3}')
+
+#Wahana Roller Coaster Tornado Dufan memiliki ketentuan bagi para pengunjung, yakni pengunjung harus memiliki tinggi badan minimal 145 cm. 
+# Selain itu, pengunjung tidak
+# diizinkan untuk menaiki wahana tersebut. Terapkan IF/ELSE dan ternary operator pada
+# kasus di atas!
+
+while True:
+    print('GABOLE YAAAAA')
+
