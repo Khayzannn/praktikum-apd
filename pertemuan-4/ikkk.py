91f49eb8b24b733438f022c1b67ea605fce3db24
Khayzannn <streamingattala@gmail.com>
2025-10-18 18:25:19 +0800

    lol

--- pertemuan-4/ikkk.py
+++ pertemuan-4/ikkk.py
@@ -1,44 +1,68 @@
-# for range
-# start 
-# for i in range (1, 20):
-#     print(f'Max Verstappen menang {i}')
+# # for range
+# # start 
+# # for i in range (1, 20):
+# #     print(f'Max Verstappen menang {i}')
 
-# #step
+# # #step
 
-# for i in range (1, 10, 3): #1 : start #10 : stop, 3: step
-#     print (f'LEKLERC{i}')
+# # for i in range (1, 10, 3): #1 : start #10 : stop, 3: step
+# #     print (f'LEKLERC{i}')
 
 
-# simpan = ['Honda', 'Accura', 8, True]
-# for i in simpan:
-#     print(i)
+# # simpan = ['Honda', 'Accura', 8, True]
+# # for i in simpan:
+# #     print(i)
 
-# for i in "REZE":
-#     print(i)
-# for i in range (2):
-#     for j in range (0, i+2):
-#         print (" stay", end=" out")
-#     print("")    
+# # for i in "REZE":
+# #     print(i)
+# # for i in range (2):
+# #     for j in range (0, i+2):
+# #         print (" stay", end=" out")
+# #     print("")    
 
-# print(" it's too late now for fvck sake")
+# # print(" it's too late now for fvck sake")
 
-#kimi raikkonen ahh team radio
+# #kimi raikkonen ahh team radio
 
-#while 
+# #while 
 
-# jawab = 'ya'
-# hitung = 0
+# # jawab = 'ya'
+# # hitung = 0
 
-# while (jawab == 'ya') :
-#     # print('ulangi')
-#     hitung += 1
-#     jawab = input('ulangi??')
-# print(f'total: {hitung}')
+# # while (jawab == 'ya') :
+# #     # print('ulangi')
+# #     hitung += 1
+# #     jawab = input('ulangi??')
+# # print(f'total: {hitung}')
 
-x = [2, 4, 6, 8, 10, 12, 14, 16]
+# x = [2, 4, 6, 8, 10, 12, 14, 16]
 
-for n in x:
-    if n > 10:
-       print(f' angka {n} lebih besar dari 10 perulangan berhenti')
-    break
-print ('done')    
+# for n in x:
+#     if n > 10:
+#        print(f' angka {n} lebih besar dari 10 perulangan berhenti')
+#     break
+# print ('done')    
+# def my_funcion(nama):
+#     print(f'{nama} ')
+
+
+# my_funcion ('Alip')
+
+def function():
+    print('''@@@@@@@
+    Hallo  
+    &&&&&&&&&&&
+    apa kabar 
+    ##''')
+function()
+
+def attol():
+    for i in range(5):
+        print('@')
+        attol()
+
+def p():
+    for i in range(1000000*90):
+        print(f'alip maniez '*12)
+    return i
+print(p())
